b4a17c981e56be00918c27c82958ef18ed112e8b
ChanwheKim <chanwhe.kim@gmail.com>
2018-08-06 18:58:36 +0900

    feat: Set up the first event listeners
    When clients click the button or press 'enter' or 'return' key on the
    browser, the application should do its tasks. In order to do that,
    I set up a click event listener and a keypress event listener.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-
+    
 
 })();
 
@@ -15,5 +15,13 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log('Button was clicked.')
+    })
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            console.log('Return-key was pressed.');
+        }
+    })
     
 })(budgetController, UIController);
