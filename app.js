91cb45fb049008b581416234ea0bf12a129d132a
ChanwheKim <chanwhe.kim@gmail.com>
2018-08-07 10:25:51 +0900

    feat: Add getInput method on the UI controller
    In order to get datas which clients input, I added getInput method.
    In this way, controller module of this application can receive
    the datas to perform its own role.

--- app.js
+++ app.js
@@ -7,7 +7,30 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+    var DOMstrings = {
+        inputBudget: '.add__budget',
+        inputDescription: '.add__description',
+        inputType: '.add__type',
+        inputQuantity: '.add__quantity',
+        inputUnitCost: '.add__unit-cost',
+        inputBtn: '.add__btn'
+    }
     
+    return {
+        getInput: function() {
+            return {
+                budget: document.querySelector(DOMstrings.inputBudget).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                type: document.querySelector(DOMstrings.inputType).value,
+                quantity: document.querySelector(DOMstrings.inputQuantity).value,
+                unitCost: document.querySelector(DOMstrings.inputUnitCost).value
+            }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings
+        }
+    }
 
 })();
 
@@ -15,24 +38,27 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
-        console.log('It works!')
-        // Get the field data input
+        // 1. Get the field data input
+        var input = UICtrl.getInput();
+        console.log(input);
 
-        // Add a new item to data structure
+        // 2. Add a new item to data structure
 
-        // Add a new item on the user interface
+        // 3. Add a new item on the user interface
 
-        // Clear the fields
+        // 4. Clear the fields
 
-        // Calculate and update budget
+        // 5. Calculate and update budget
 
-        // Calculate and update percentage
+        // 6. Calculate and update percentage
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
             ctrlAddItem();
